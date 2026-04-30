47a85fd07ab89fd53a64e031cd5d66ac51aae64c
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-30 09:30:55 +0530

    feat(identity) : add DapperContext
    -add ConnectionString

--- src/services/MedFlow.Identity/MedFlow.Identity.Infrastructure/Persistence/Data/DapperContext.cs
+++ src/services/MedFlow.Identity/MedFlow.Identity.Infrastructure/Persistence/Data/DapperContext.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
 
 namespace MedFlow.Identity.Infrastructure.Persistence.Data
 {
     public class DapperContext
     {
         private readonly string _conString;
-        public DapperContext(IConfiguration config) { }
+        public DapperContext(IConfiguration config) 
+        {
+            _conString = config.GetConnectionString("DefaultConnection") ?? throw new ArgumentNullException(nameof(config));
+        }
+
+        public  IDbConnection Createconnection() 
+            => new SqlConnection(_conString);
     }
 }
