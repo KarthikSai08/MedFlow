1dc156e01c40deadb6d44bb562fc7074d719a871
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-05-01 13:35:39 +0530

    feat : add helper Methods to
    -generate RefreshToken
    -Hash & Verify passwords

---
+++ src/services/MedFlow.Identity/MedFlow.Identity.Application/Helper/PasswordHasher.cs
@@ -0,0 +1,25 @@
+﻿using MedFlow.Identity.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedFlow.Identity.Application.Helper
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+
+        public string Hash(string password)
+        {
+            var hashedPass = BCrypt.Net.BCrypt.HashPassword(password);
+
+            return hashedPass;
+        }
+
+        public bool Verify(string password, string hashPassword)
+        {
+            var success = BCrypt.Net.BCrypt.Verify(password, hashPassword);
+            return success;
+        }
+        
+    }
+}
---
+++ src/services/MedFlow.Identity/MedFlow.Identity.Application/Helper/RefreshTokenGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MedFlow.Identity.Application.Helper
+{
+    public class RefreshTokenGenerator()
+    {
+        public string GenerateRefreshToken()
+        {
+            var bytes = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(bytes);
+
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
